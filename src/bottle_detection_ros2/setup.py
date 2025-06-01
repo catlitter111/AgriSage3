c4468289d10bbdd2bd0c325619ae1e2725dfeece
YourUsername <youremail@example.com>
2025-06-01 15:59:53 +0800

    build_success

--- src/bottle_detection_ros2/setup.py
+++ src/bottle_detection_ros2/setup.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'bottle_detection_ros2'
 
 setup(
     name=package_name,
     version='1.0.0',
-    packages=find_packages(),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -18,7 +20,7 @@ setup(
     zip_safe=True,
     maintainer='Your Name',
     maintainer_email='your_email@example.com',
-    description='ROS2 package for bottle detection and harvesting',
+    description='ROS2 nodes for bottle detection and harvesting system',
     license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
@@ -30,6 +32,7 @@ setup(
             'robot_control_node = bottle_detection_ros2.robot_control_node:main',
             'servo_control_node = bottle_detection_ros2.servo_control_node:main',
             'auto_harvest_controller = bottle_detection_ros2.auto_harvest_controller:main',
+            'test_system = bottle_detection_ros2.test_system:main',
         ],
     },
 )
